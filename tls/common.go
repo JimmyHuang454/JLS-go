17fba45478ad2d1f4939fcb95e8b59213bca3697
JimmyHuang <jimmyhuang454@gmail.com>
2023-08-23 20:06:11 +0800

    move to a single file

--- tls/common.go
+++ tls/common.go
@@ -23,8 +23,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/JimmyHuang454/JLS-go/jls"
 )
 
 const (
@@ -789,130 +787,6 @@ type Config struct {
 	UseJLS bool
 	JLSPWD []byte
 	JLSIV  []byte
-}
-
-func JLSHandler(c *Conn, tlsError error) error {
-	if !c.config.UseJLS {
-		return tlsError
-	}
-
-	if c.isClient {
-		if tlsError == nil && !c.IsValidJLS {
-			// it is a valid TLS Client but Not JLS,
-			defer c.Close()
-			return errors.New("not JLS")
-			// so we must TODO: act like a normal http request at here
-		}
-	} else if tlsError != nil && !c.IsValidJLS {
-		// It is not JLS. Forward at here.
-		// TODO: if we using sing-box, we need to use its forward method, since it may take over traffic by Tun.
-		server, forwardError := net.Dial("tcp", c.config.ServerName+":443")
-		fmt.Println(c.config.ServerName + ":443 forwarding...")
-		if forwardError == nil {
-			defer server.Close()
-			defer c.conn.Close()
-			server.Write(c.ClientHelloRecord)
-			server.Write(c.ForwardClientHello)
-			go io.Copy(server, c.conn)
-			io.Copy(c.conn, server)
-		}
-	}
-	return tlsError
-}
-
-func BuildJLSClientHello(c *Conn, hello *clientHelloMsg) {
-	if !c.config.UseJLS || c.IsBuildedFakeRandom {
-		return
-	}
-	zeroArray := BuildZeroArray()
-	hello.random = zeroArray
-	withoutBinder, _ := hello.marshalWithoutBinders()
-	hello.random, _ = BuildFakeRandom(c.config, withoutBinder)
-	copy(hello.raw[6:], hello.random)
-	c.IsBuildedFakeRandom = true
-}
-
-func BuildJLSServerHello(c *Conn, hello *serverHelloMsg) {
-	if !c.config.UseJLS {
-		return
-	}
-
-	hello.random = BuildZeroArray()
-	hello.marshal()
-
-	hello.random, _ = BuildFakeRandom(c.config, hello.raw)
-	copy(hello.raw[6:], hello.random)
-}
-
-func CheckJLSServerHello(c *Conn, serverHello *serverHelloMsg) {
-	c.IsValidJLS = false
-	if !c.config.UseJLS {
-		return
-	}
-	serverHello.marshal() // init
-	zeroArray := BuildZeroArray()
-	raw := make([]byte, len(serverHello.raw))
-	copy(raw, serverHello.raw)
-	copy(raw[6:], zeroArray)
-
-	c.IsValidJLS, _ = CheckFakeRandom(c.config, raw, serverHello.random)
-	c.config.InsecureSkipVerify = c.IsValidJLS
-}
-
-// return false means need to forward.
-func CheckJLSClientHello(c *Conn, clientHello *clientHelloMsg) (bool, error) {
-	c.IsValidJLS = false
-	if !c.config.UseJLS {
-		return true, errors.New("disable JLS.") // == TLS.
-	}
-	zeroArray := BuildZeroArray()
-	withoutBinder, err := clientHello.marshalWithoutBinders()
-	c.ForwardClientHello = clientHello.raw
-	if err != nil {
-		return false, errors.New("failed to get clientHello raw bytes.")
-	}
-	raw := make([]byte, len(withoutBinder))
-	copy(raw, withoutBinder)
-	copy(raw[6:], zeroArray)
-
-	c.IsValidJLS, err = CheckFakeRandom(c.config, raw, clientHello.random)
-	if err != nil {
-		return false, errors.New("failed to check fakeRandom.")
-	}
-	if !c.IsValidJLS || c.vers != VersionTLS13 {
-		return false, errors.New("wrong fakeRandom.")
-	}
-	if len(clientHello.keyShares) == 0 {
-		fmt.Println("JLS missing keyShare can be not safty.")
-	}
-	return true, nil // valid JLS.
-}
-
-func BuildZeroArray() []byte {
-	const byteLen = 32
-	zeroArray := make([]byte, byteLen)
-	for i := 0; i < byteLen; i++ {
-		zeroArray[i] = 0
-	}
-	return zeroArray
-}
-
-func BuildFakeRandom(config *Config, AuthData []byte) ([]byte, error) {
-	iv := append(config.JLSIV, AuthData...)
-	pwd := append(config.JLSPWD, AuthData...)
-	fakeRandom := jls.NewFakeRandom(pwd, iv)
-
-	err := fakeRandom.Build()
-	return fakeRandom.Random, err
-}
-
-func CheckFakeRandom(config *Config, AuthData []byte, random []byte) (bool, error) {
-	iv := append(config.JLSIV, AuthData...)
-	pwd := append(config.JLSPWD, AuthData...)
-	fakeRandom := jls.NewFakeRandom(pwd, iv)
-
-	IsValid, err := fakeRandom.Check(random)
-	return IsValid, err
 }
 
 const (
